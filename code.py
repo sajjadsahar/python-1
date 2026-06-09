4111073a2cafd859087d938fca898054f04872d2
Sajjad Sahar <65441@students.riphah.edu.pk>
2026-06-09 03:49:18 -0700

    Initial commit

---
+++ code.py
@@ -0,0 +1,43 @@
+# print("Hellow world");
+
+# 1 variables in python
+
+# name = "sajjad";
+# age = 22;
+# _PI = 3.14;
+
+# print("Your name is ",name);
+# print("your age is ",age-1);
+# print(_PI);
+
+# 2 Datatypes in python 
+
+# name = "sajjad";
+# age = 22;
+# PI = 3.14;
+
+# print(type(name));
+# print(type(age));
+# print(type(PI));
+
+'''
+this is a multi line comment in python 
+'''
+
+# Arthmetic opration 
+
+a = 10
+b = 5
+
+sum = a + b
+mul = a * b
+div = a / b
+
+print(sum)
+print(mul)
+print(div)
+print(a%b)  
+print(a**b)  
+print(a<b)  
+print(a==b)  
+print(a!=b)  
